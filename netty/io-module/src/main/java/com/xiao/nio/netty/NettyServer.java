110e55cf2178827d9db7814e3a946e80b46000f5
xiao xiao <you@example.com>
2020-09-13 19:45:07 +0800

    first commit

--- netty/io-module/src/main/java/com/xiao/nio/netty/NettyServer.java
+++ netty/io-module/src/main/java/com/xiao/nio/netty/NettyServer.java
@@ -37,7 +37,7 @@ public class NettyServer {
                 .childHandler(new ChannelInitializer<SocketChannel>() {
                     @Override
                     protected void initChannel(SocketChannel socketChannel) throws Exception {
-                        socketChannel.pipeline().addLast(new NettyServerHandler());
+                        socketChannel.pipeline().addLast(new NettyServerExcuteHandler()).addLast();
                     }
                 });
         //绑定一个端口并且同步
