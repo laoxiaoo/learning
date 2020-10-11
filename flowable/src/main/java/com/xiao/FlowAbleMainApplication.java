caf0150e81b66fb5e964b5177cd501e79ada1ba0
xiao xiao <you@example.com>
2020-10-11 23:54:17 +0800

    flowable

---
+++ flowable/src/main/java/com/xiao/FlowAbleMainApplication.java
@@ -0,0 +1,17 @@
+package com.xiao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @program: learning
+ * @description: TODO
+ * @author: lonely xiao
+ * @create: 2020-10-11 11:46
+ */
+@SpringBootApplication
+public class FlowAbleMainApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FlowAbleMainApplication.class, args);
+    }
+}
