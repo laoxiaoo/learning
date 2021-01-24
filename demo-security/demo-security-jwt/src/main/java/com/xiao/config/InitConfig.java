edd4e312adf2e8f232d8d91d99924322bd32e506
xiao xiao <2518127495@qq.com>
2021-01-24 19:39:37 +0800

    security

--- demo-security/demo-security-jwt/src/main/java/com/xiao/config/InitConfig.java
+++ demo-security/demo-security-jwt/src/main/java/com/xiao/config/InitConfig.java
@@ -1,8 +1,22 @@
-package com.xiao.config;/**
- * @ClassName InitConfig.java
+package com.xiao.config;
+
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+
+/**
  * @author lonely xiao
  * @version 1.0
- * @Description 
+ * @ClassName InitConfig.java
+ * @Description
  * @createTime 2021年01月24日 19:37:00
- */public class InitConfig {
+ */
+@Component
+public class InitConfig {
+
+
+    @PostConstruct
+    public void init() {
+
+    }
 }
--- demo-security/demo-security-jwt/src/main/java/com/xiao/entity/TPermission.java
+++ demo-security/demo-security-jwt/src/main/java/com/xiao/entity/TPermission.java
@@ -1,8 +1,24 @@
-package com.xiao.entity;/**
- * @ClassName TPermission.java
+package com.xiao.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+/**
  * @author lonely xiao
  * @version 1.0
- * @Description 
+ * @ClassName TPermission.java
+ * @Description
  * @createTime 2021年01月24日 19:35:00
- */public class TPermission {
+ */
+@ToString
+@Getter
+@Setter
+public class TPermission {
+
+    private Long id;
+
+    private String permissionCode;
+
+    private String permissionName;
 }
