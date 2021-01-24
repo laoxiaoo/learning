743cd3b9498b923d970e8e15ac41646b7d49e34a
xiao xiao <2518127495@qq.com>
2021-01-24 19:39:31 +0800

    security

---
+++ demo-security/demo-security-jwt/src/main/java/com/xiao/config/InitConfig.java
@@ -0,0 +1,8 @@
+package com.xiao.config;/**
+ * @ClassName InitConfig.java
+ * @author lonely xiao
+ * @version 1.0
+ * @Description 
+ * @createTime 2021年01月24日 19:37:00
+ */public class InitConfig {
+}
---
+++ demo-security/demo-security-jwt/src/main/java/com/xiao/entity/TPermission.java
@@ -0,0 +1,8 @@
+package com.xiao.entity;/**
+ * @ClassName TPermission.java
+ * @author lonely xiao
+ * @version 1.0
+ * @Description 
+ * @createTime 2021年01月24日 19:35:00
+ */public class TPermission {
+}
