aac486e3ebc1e04f4e985ac5abdbbeeab6ab4120
xiao xiao <2518127495@qq.com>
2021-02-09 16:26:06 +0800

    spring

---
+++ stu-spring/src/main/java/com/xiao/in/spring/dependency/QualifierDependencyDemo.java
@@ -0,0 +1,17 @@
+package com.xiao.in.spring.dependency;
+
+/**
+ * @author lonely xiao
+ * @version 1.0
+ * @ClassName QualifireDependencyDemo.java
+ * @Description
+ * @createTime 2021年02月09日 16:12:00
+ */
+public class QualifierDependencyDemo {
+
+
+    public static void main(String[] args) {
+
+    }
+
+}
