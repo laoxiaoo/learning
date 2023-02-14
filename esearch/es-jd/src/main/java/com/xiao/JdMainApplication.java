faf2f3fc79e76ecab6159cb878d5af3f6122b361
xiaojihao <2518127495@qq.com>
2023-02-14 13:38:28 +0800

    es

--- esearch/es-jd/src/main/java/com/xiao/JdMainApplication.java
+++ esearch/es-jd/src/main/java/com/xiao/JdMainApplication.java
@@ -2,14 +2,18 @@ package com.xiao;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.elasticsearch.ElasticsearchDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.elasticsearch.ElasticsearchRestClientAutoConfiguration;
 
 /**
- * @program: learning
- * @description: TODO
- * @author: lonely xiao
+ * 需要将
+ *  ElasticsearchDataAutoConfiguration.class, ElasticsearchRestClientAutoConfiguration.class
+ *  屏蔽，不然可能spring boot自动加载，可能产生不可预知的错误
+ *
+ * @author: lao xiao
  * @create: 2020-10-08 15:06
  */
-@SpringBootApplication
+@SpringBootApplication(exclude = { ElasticsearchDataAutoConfiguration.class, ElasticsearchRestClientAutoConfiguration.class})
 public class JdMainApplication {
 
     public static void main(String[] args) {
