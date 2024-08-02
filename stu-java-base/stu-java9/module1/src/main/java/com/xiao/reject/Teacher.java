6e039b097fe1f0779841556a7cb22acdb376f634
xiaojihao <2518127495@qq.com>
2024-08-02 14:18:18 +0800

    java9

---
+++ stu-java-base/stu-java9/module1/src/main/java/com/xiao/reject/Teacher.java
@@ -0,0 +1,8 @@
+package com.xiao.reject;
+
+/**
+ * @author lao xiao
+ * @date 2024-08-02 11:10
+ */
+public class Teacher {
+}
---
+++ stu-java-base/stu-java9/module2/src/main/java/com/xiao/Main.java
@@ -0,0 +1,16 @@
+package com.xiao;
+
+import com.xiao.allow.Student;
+
+/**
+ * @author lao xiao
+ * @date 2024-08-02 11:11
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        Student student = new Student();
+        System.out.println(student);
+    }
+
+}
